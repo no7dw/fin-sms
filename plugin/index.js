8be2720d78f68a91ecf35b16985ae1df43ce4c93
no7david <no7david@gmail.com>
2015-08-09 01:01:40 +0800

    add plugin for maixuntong , test pass

--- plugin/index.js
+++ plugin/index.js
@@ -3,17 +3,24 @@
  */
 //defend = require('./defend');
 //luoSiMao = require('./luosimao');
+//var Plugin = require('./plugin');
+//var maiXunTong = new Plugin('maixutong');
+
 var MaiXunTong = require('./maixuntong');
-var maiXunTong = new maiXunTong({});
+var mxtConfig = {};
+var maiXunTongObj = new MaiXunTong(mxtConfig);
 
 module.exports.send = function(phone, message ,callback){
   var phoneForLuoSiMao = /^(133|189|155|177|181)\d{8}$/;
   if(phoneForLuoSiMao.test(phone)){
-    maiXunTong.send();
+    console.log('maixuntong');
+    maiXunTongObj.send(phone, message,callback);
     callback(null, null);
   }
   else
   {
+    console.log('$$ luosimao');
+    maiXunTongObj.send(phone, message,callback);
     callback(null, null);
   }
 };
--- plugin/maixuntong.js
+++ plugin/maixuntong.js
@@ -1,42 +1,46 @@
 var querystring = require('querystring');
 
-function maiXunTong(config) {
-  //麦讯通请求url前缀
-  this.urlPre = 'http://www.mxtong.net.cn/GateWay/Services.asmx/DirectSend?';
-  this.config = config;
-};
-
-maiXunTong.prototype.send = function (phone, message, cb) {
-  var self = this;
-  if (true) {
-    console.log('### dev env, message not send');
-    return callback(null, null);
-  }
-
-  //麦讯通请求url参数
-  var urlObject = {
-    "UserID": "",
-    "Account": "",
-    "Password": "",
-    "Phones": phone + ';', //分号不能省略
-    "Content": message,
-    "SendTime": "",
-    "SendType": 1,
-    "PostFixNumber": ""
+MaiXunTong = (function() {
+  function MaiXunTong(config) {
+    //麦讯通请求url前缀
+    this.urlPre = 'http://www.mxtong.net.cn/GateWay/Services.asmx/DirectSend?';
+    this.config = config;
   };
+  MaiXunTong.prototype.send = function (phone, message, cb) {
+    console.log('### send', phone, message);
+    var self = this;
+    if (true) {
+      console.log('### dev env, message not send');
+      return cb(null, null);
+    }
 
-  request
-    .get(self.config.urlPre + querystring.stringify(urlObject))
-    .end(function (err, res) {
-      console.log('send result', res.text);
-      var sendResult = /Sucess/.test(res.text);
-      if (!err && sendResult) {
-        console.log('-------------------------send message success-----------------------------');
-      } else {
-        //todo : emit an error
-        console.log('send message fail');
-      }
-      cb(err, sendResult);
-    });
-};
-exports = maiXunTong;
+    //麦讯通请求url参数
+    var urlObject = {
+      "UserID": "",
+      "Account": "",
+      "Password": "",
+      "Phones": phone + ';', //分号不能省略
+      "Content": message,
+      "SendTime": "",
+      "SendType": 1,
+      "PostFixNumber": ""
+    };
+
+    request
+      .get(self.config.urlPre + querystring.stringify(urlObject))
+      .end(function (err, res) {
+        console.log('send result', res.text);
+        var sendResult = /Sucess/.test(res.text);
+        if (!err && sendResult) {
+          console.log('-------------------------send message success-----------------------------');
+        } else {
+          //todo : emit an error
+          console.log('send message fail');
+        }
+        cb(err, sendResult);
+      });
+  };
+  return MaiXunTong;
+})();
+
+module.exports =  MaiXunTong;
