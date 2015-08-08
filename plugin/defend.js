3b5d71e3c9a257f0c27d75cd8ad9ab6cfdc94af8
no7david <no7david@gmail.com>
2015-08-08 19:24:50 +0800

    add package.json

--- plugin/defend.js
+++ plugin/defend.js
@@ -23,4 +23,66 @@ defend.prototype.filter(ip, phone, action, cb){
       });
     }
   });
-}
+};
+defend.prototype.checkTooMuch(ip, phone, action, callback){
+  if(_.contains(skipIp.denylist, clientIP)){
+    sails.log.debug('$$$ 请求过于频繁!' ,clientIP, phone);
+    return res.json(403, {data:"请求过于频繁"});
+  }
+
+  async.waterfall([
+    function (cb) {
+      Token.native(function (err, collection) {
+        if (err) {
+          cb(err);
+        } else {
+          collection.GET(clientIP, function (err, data) {
+            cb(err, data);
+          });
+        }
+      });
+    },
+    function (count, cb) {
+      sails.log.debug('sendMessage', count);
+      if (!count) {
+        Token.native(function (err, collection) {
+          if (err) {
+            cb(err);
+          } else {
+            collection.SETEX(clientIP, 86400, '0', function (err, data) {
+              sails.log.debug("message: SETEX ", clientIP, data);
+              cb(err, data);
+            });
+          }
+        });
+      } else {
+        Token.native(function (err, collection) {
+          if (err) {
+            cb(err);
+          } else {
+            collection.INCR(clientIP, function (err, data) {
+              sails.log.debug("message: INCR ", clientIP, data);
+              cb(err, data);
+            });
+          }
+        });
+      }
+    },
+    function (status, cb) {
+      sails.log.debug('sendMessage', status);
+      Token.native(function (err, collection) {
+        if (err) {
+          cb(err);
+        } else {
+          collection.GET(clientIP, function (err, data) {
+            cb(err, data);
+          });
+        }
+      });
+    }
+  ], function (err, result) {
+    sails.log.debug('$$$ sendMessage result ', clientIP, phone, result, "err", err);
+    callback(err, result);
+  });
+};
+module.exports = defend;
